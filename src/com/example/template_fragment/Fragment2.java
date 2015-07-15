3818524b5afad947ad746446a826e9ed556247d4
sephirothz87 <sephiroth.zhong@gmail.com>
2015-07-15 16:18:01 +0800

    test remote commit
    test remote commit

--- src/com/example/template_fragment/Fragment2.java
+++ src/com/example/template_fragment/Fragment2.java
@@ -15,7 +15,7 @@ public class Fragment2 extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-		//cc
+		//cc 123456
 		return inflater.inflate(R.layout.fragment_2, null);
 	}
 	
