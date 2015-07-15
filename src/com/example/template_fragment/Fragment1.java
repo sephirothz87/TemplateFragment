ab20ef86aac25614951b145dd8328c22641dad4f
sephirothz87 <sephiroth.zhong@gmail.com>
2015-07-15 16:14:52 +0800

    commit test

--- src/com/example/template_fragment/Fragment1.java
+++ src/com/example/template_fragment/Fragment1.java
@@ -9,10 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Fragment1 extends Fragment {
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// comment
 		return inflater.inflate(R.layout.fragment_1, null);
 	}
-	
+
 }
